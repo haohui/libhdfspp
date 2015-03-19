78beeab3d0678c3cb7380e934e1b70aa4ebdf547
Haohui Mai <wheat9@apache.org>
2015-03-18 17:45:31 -0700

    Implement monads for asio::asio_connect() and asio::async_resolve().

--- src/main/native/lib/common/monad/asio.h
+++ src/main/native/lib/common/monad/asio.h
@@ -23,8 +23,10 @@
 
 #include "libhdfs++/status.h"
 
+#include <asio/connect.hpp>
 #include <asio/read.hpp>
 #include <asio/write.hpp>
+#include <asio/ip/tcp.hpp>
 
 namespace hdfs {
 namespace monad {
@@ -73,6 +75,59 @@ struct WriteMonad : Monad<> {
   BufferOp get_buffer_;
 };
 
+template<class Socket, class Iterator>
+struct ConnectMonad : Monad<> {
+  ConnectMonad(Socket *socket, Iterator begin, Iterator end, Iterator *connected_endpoint)
+      : socket_(socket)
+      , begin_(begin)
+      , end_(end)
+      , connected_endpoint_(connected_endpoint)
+  {}
+
+  HDFS_MONAD_DEFAULT_MOVE_CONSTRUCTOR(ConnectMonad);
+  HDFS_MONAD_DISABLE_COPY_CONSTRUCTOR(ConnectMonad);
+
+  template<class Next>
+  void Run(const Next& next) {
+    auto handler = [this,next](const asio::error_code &ec, Iterator it) {
+      if (connected_endpoint_) {
+        *connected_endpoint_ = it;
+      }
+      next(ToStatus(ec));
+    };
+    asio::async_connect(*socket_, begin_, end_, handler);
+  }
+ private:
+  Socket *socket_;
+  Iterator begin_;
+  Iterator end_;
+  Iterator *connected_endpoint_;
+};
+
+struct ResolveMonad : Monad<> {
+  ResolveMonad(::asio::io_service *io_service,
+               const std::string &host,
+               const std::string &service)
+      : resolver_(*io_service)
+      , query_(host, service)
+  {}
+
+  HDFS_MONAD_DEFAULT_MOVE_CONSTRUCTOR(ResolveMonad);
+  HDFS_MONAD_DISABLE_COPY_CONSTRUCTOR(ResolveMonad);
+
+  template<class Next>
+  void Run(const Next& next) {
+    auto handler = [next](const asio::error_code &ec,
+                          asio::ip::tcp::resolver::iterator it)
+                   { next(ToStatus(ec), it); };
+    resolver_.async_resolve(query_, handler);
+  }
+
+ private:
+  ::asio::ip::tcp::resolver resolver_;
+  ::asio::ip::tcp::resolver::query query_;
+};
+
 namespace detail {
 
 template<class Buffer>
@@ -93,25 +148,36 @@ template<class Stream, class MutableBufferSequence>
 using EarlyBindReadOperation = ReadMonad<Stream, detail::EarlyBufferBinder<MutableBufferSequence> >;
 
 template<class Stream, class BufferOp>
-WriteMonad<Stream, BufferOp> LateBindWrite(Stream *stream, const BufferOp &get_buffer) {
+static inline WriteMonad<Stream, BufferOp> LateBindWrite(Stream *stream, const BufferOp &get_buffer) {
   return WriteMonad<Stream, BufferOp>(stream, get_buffer);
 }
 
 template<class Stream, class ConstBufferSequence>
 EarlyBindWriteOperation<Stream, ConstBufferSequence>
-Write(Stream *stream, const ConstBufferSequence &buffer) {
+static inline Write(Stream *stream, const ConstBufferSequence &buffer) {
   return LateBindWrite(stream, detail::EarlyBufferBinder<ConstBufferSequence>(buffer));
 }
 
 template<class Stream, class BufferOp>
-ReadMonad<Stream, BufferOp> LateBindRead(Stream *stream, const BufferOp &get_buffer) {
+static inline ReadMonad<Stream, BufferOp> LateBindRead(Stream *stream, const BufferOp &get_buffer) {
   return ReadMonad<Stream, BufferOp>(stream, get_buffer);
 }
 
 template<class Stream, class MutableBufferSequence>
 EarlyBindReadOperation<Stream, MutableBufferSequence>
-Read(Stream *stream, const MutableBufferSequence &buffer) {
+static inline Read(Stream *stream, const MutableBufferSequence &buffer) {
   return LateBindRead(stream, detail::EarlyBufferBinder<MutableBufferSequence>(buffer));
+}
+
+static inline ResolveMonad &&Resolve(::asio::io_service *io_service, const std::string &host,
+                    const std::string &service) {
+  return std::move(Resolve(io_service, host, service));
+}
+
+template<class Socket, class Iterator>
+ConnectMonad<Socket, Iterator>
+static inline Connect(Socket *socket, Iterator begin, Iterator end) {
+  return ConnectMonad<Socket, Iterator>(socket, begin, end, nullptr);
 }
 
 }
